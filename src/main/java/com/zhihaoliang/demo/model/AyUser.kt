409a15da6e5b60435ac24abb704d8ce362e53af9
小支同学 <zhihaoliang07@163.com>
2019-09-30 11:05:49 +0800

    frist init

---
+++ src/main/java/com/zhihaoliang/demo/model/AyUser.kt
@@ -0,0 +1,10 @@
+package com.zhihaoliang.demo.model
+
+/**
+ *
+ * @author zhihaoliang
+ * @date 2019/9/2911:33 AM
+ * @version：1.0.0
+ */
+
+data class AyUser (val id:String,val name:String,val password:String)
